5dad4c5f7f7fd4fc6c73e7d9a74c8eae420925a9
timmeubel <timvandermeulen110@gmail.com>
2026-06-05 08:50:46 +0200

    bugs
    bugs gefixt boem

--- SRWW/database/migrations/2026_06_04_073130_add_timestamps_to_user_table.php
+++ SRWW/database/migrations/2026_06_04_073130_add_timestamps_to_user_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('user', function (Blueprint $table) {
+            //
         });
     }
 
@@ -22,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user');
+        Schema::table('user', function (Blueprint $table) {
+            //
+        });
     }
 };
