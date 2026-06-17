438bfa4b2d15ca60392a267c142fa76428c8274e
Vincent6777 <posthumavincent@gmail.com>
2026-06-17 08:54:55 +0200

    maakt de site niet vindbaar door een random persoon

--- SRWW/resources/views/verify-email.blade.php
+++ SRWW/resources/views/verify-email.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
     <title>Verifieer je e-mail</title>
     <link rel="stylesheet" href="{{ asset('css/variables.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
